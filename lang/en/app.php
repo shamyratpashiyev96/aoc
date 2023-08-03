446c9147b9636f2cd6a71e9eab1d7d2c3281c1b2
shamurat <it.shamurat@st.com.tm>
2023-08-03 15:24:46 +0500

    Finished translation

---
+++ lang/en/app.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+    'layout' => [
+        'logo_text' => 'Administration of the Olympic Village<br/> of government of Ashgabat ',
+        'main_page' => 'Main',
+        'about_us_page' => 'About Us',
+        'news_page' => 'News',
+        'gallery_page' => 'Gallery',
+        'contacts_page' => 'Contacts',
+
+        'phone_key' => 'PHONE',
+        'fax_key' => 'FAX',
+        'email_key' => 'E-MAIL',
+        'copyright_text' => 'ALL RIGHTS RESERVED'
+    ],
+
+    'main_page' => [
+        'banner_text_1' => 'port is a source of strength, beauty<br/> and health',
+        'banner_text_2' => '«The year of happy youth<br/> with Arkadag Serdar',
+        'flipcard_1' => [
+            'title' => 'About the olympic city',
+            'desc' => 'The unique facility, which has no analogues in the Central Asian 
+                        region, has became the venue for the Asian Indoor games, it includes 
+                        more than 30 facilities for various purposes including 
+                        the Olympic stadium, the paralympic complex and the 
+                        rehabilitation medical center. The initiator of 
+                        the construction is the President of Turkmenistan 
+                        Gurbanguly Berdimuhamedov. It hosted the opening 
+                        and closing ceremonies of the games, all 
+                        competitions, as well as the awards ceremony'
+        ],
+        'flipcard_2' => [
+            'title' => 'Sport complexes',
+            'desc' => '<div class="flipcards__left_column flex_col">
+                            <span class="bold">Facility</span>
+                            <span>Stadium «Olympic»</span>
+                            <span>Indoor cycle track </span>
+                            <span>Martial Arts sports complex </span>
+                            <span>Indoor athletics arena </span>
+                            <span>Large multifunctional hall </span>
+                            <span>Indoor tennis court </span>
+                            <span>Indoor pool</span>
+                            <span>Multifunctional hall</span>
+                            <span>Indoor arena</span>
+                        </div>
+                        <div class="flipcards__right_column flex_col">
+                            <span class="bold">Capacity</span>
+                            <span>4500</span>
+                            <span>6000</span>
+                            <span>5000</span>
+                            <span>5000</span>
+                            <span>3000</span>
+                            <span>4000</span>
+                            <span>5000</span>
+                            <span>700</span>
+                            <span>15 000</span>
+                        </div>'
+        ],
+        'flipcard_3' => [
+            'title' => 'HOTELS',
+            'desc' => '<span class="bold">«Olympiya» hotel capacity 450</span>
+                        <p>
+                            Presidential suite - 1; standart single rooms - 136; Standard double rooms - 36; suites - 45; apartaments- 5; disable rooms - 2; 5 conference hall - 114; 2 restaurants - 256; bar -58; lounge bar - 71; bakery - 45; SPA complex
+                        </p>
+                        <hr/>
+                        <span class="bold">Hotel “Sport” capacity 800</span>
+                        <p>
+                            Presidential suites - 2; standart single rooms - 228; Standard double rooms - 108; suites- 48; apartaments - 64; disable rooms - 8; conference hall - 700; 4 restaurants - 1030; 2 cafes - 178; SPA complex
+                        </p>'
+        ],
+        'flipcard_4' => [
+            'title' => 'Infrastructure',
+            'desc' => '<span class="bold">Monorail length - 5 138 метров</span>
+                        <p>
+                            Heigth  6 meters to 13 ; 8 stations; capacity of one train is 70 person; speed 46km/h (maximum speed is 70 km/h); 
+                        </p>
+                        <hr />
+                        <span class="bold">Medical and recreation center</span>
+                        <p>
+                            MR-angiography; ultrasonic investigation; ECG; reflexelogy; vacuum therapy; galvanotherapy; physoitherapy; electrotherapy; dopplerography. labaratory; gymnastic hall; 8 chambers; 1 massage room;  1 pool ; 1 cafe.
+                        </p>'
+        ],
+        'about_us_section' => [
+            'title' => 'ABOUT OLYMPIC CITY',
+            'desc' => 'По поручению нашего уважаемого Президента, в нашей стране проведена масштабная работа по развитию физкультурно-оздоровительного движения, массового спорта и спортсменов с высокими достижениями, подготовке туркменских спортсменов к крупным международным соревнованиям, город Ашхабад полностью соответствует международным стандартам, не имеет аналогов во всем регионе и является ярким символом Государственной политики, по обеспечению благополучного и стабильного образа жизни своего народа. Олимпийская деревня стала особым украшением нашего мраморного города, образуя свое неповторимое единство с неповторимой своей красотой и другими объектами спорта и физкультуры, расположенными в нем. В декабре 2017 года была создана Администрация Олимпийского города администрации города Ашхабада.',
+        ],
+        'gallery_section' => 'PHOTOGALLERY',
+        'news_section' => 'News',
+    ],
+];
--- lang/tm/app.php
+++ lang/tm/app.php
@@ -13,5 +13,78 @@ return [
         'fax_key' => 'FAKS',
         'email_key' => 'E-MAIL',
         'copyright_text' => 'ÄHLI HUKUKLAR GORALAN'
-    ]
+    ],
+    'main_page' => [
+        'banner_text_1' => 'Sport güýç, gözellik<br/> we saglyk çeşmesidir',
+        'banner_text_2' => '«Arkadag Serdarly<br/> bagtyýar ýaşlar ýyly»',
+        'flipcard_1' => [
+            'title' => 'Olimpiýa şäherçesi barada ',
+            'desc' => 'Merkezi Aziýa sebitinde özboluşly desga, ýapyk Aziýa 
+                        oýunlarynyň geçiriljek ýerine öwrüldi, ol Olimpiýa 
+                        stadiony, Paralimpiýa toplumy we Reabilitasiýa 
+                        lukmançylyk merkezi ýaly dürli maksatly 30-dan gowrak 
+                        desgany öz içine alýar. Gurluşygyň başlangyjy 
+                        Türkmenistanyň Hormatly prezidenti Gurbanguly 
+                        Berdimuhamedow. Şäherçede oýunlaryň açylyş we 
+                        ýapylyş dabaralary, ähli ýaryşlar, şeýle hem baýrak 
+                        gowşurylyş dabarasy geçirildi.'
+        ],
+        'flipcard_2' => [
+            'title' => 'Sport toplumlary',
+            'desc' => '<div class="flipcards__left_column flex_col">
+                            <span class="bold">Obýekt</span>
+                            <span>«Olimpiýa» stadiony</span>
+                            <span>Içerki aýlaw ýoly </span>
+                            <span>Söweş sungaty sport toplumy</span>
+                            <span>Içerki ýeňil atletika meýdançasy</span>
+                            <span>Uly köpugurly zal</span>
+                            <span>Içerki tennis korty</span>
+                            <span>Içerki howuz (basseýn)</span>
+                            <span>Köpugurly zal</span>
+                            <span>Içerki arena</span>
+                        </div>
+                        <div class="flipcards__right_column flex_col">
+                            <span class="bold">Sygymlylygy</span>
+                            <span>4500</span>
+                            <span>6000</span>
+                            <span>5000</span>
+                            <span>5000</span>
+                            <span>3000</span>
+                            <span>4000</span>
+                            <span>5000</span>
+                            <span>700</span>
+                            <span>15 000</span>
+                        </div>'
+        ],
+        'flipcard_3' => [
+            'title' => 'Myhmanhanalar',
+            'desc' => '<span class="bold">«Olimpiýa» myhmanhanasynyň sygymlylygy 450</span>
+                        <p>
+                            Prezident lýuksy - 1; bir orunlyk otag - 136;  iki orunlyk otag  - 36; lýuks otag - 45; apartamentler - 5; mümkinçilikleri çäklendirilen adamlar üçin otaglar - 2; 5 sany mejlisler zaly - 114; 2 sany restoran - 256; bar -58; laundj bar - 71; konditer önümleri kafesi- 45; SPA merkezi
+                        </p>
+                        <hr/>
+                        <span class="bold">«Sport» myhmahanasynyň sygymlylygy 800</span>
+                        <p>
+                            Prezident lýuksy  - 2; 1 orunlyk otag - 228; 2 orunlyk otag - 108; lýuks otag - 48; apartamentler - 64; mümkinçilikleri çäklendirilen adamlar üçin otaglar - 8; mejlisler zaly - 700; 4 sany restoran - 1030; 2 sany kafe - 178; SPA merkezi
+                        </p>'
+        ],
+        'flipcard_4' => [
+            'title' => 'Infrastruktura',
+            'desc' => '<span class="bold">Monorels uzynlygy - 5 138 метров</span>
+                        <p>
+                            Beýiklihi 6 m-dan 13 m çenli; 8 sany duralga; bir otlynyň sygymlylygy 70 adam; tizligi 46km/sag (iň ýokary ttizlik 70 km/sag); 
+                        </p>
+                        <hr />
+                        <span class="bold">Saglygy dikeldiş merkezi</span>
+                        <p>
+                            MRT; UZI; EKG; refleksoterapiýa; wakum terapiýa; galwan terapiýa; fizioterapiýa; elektroterapiýa; doplerografiýa. labaratoriýa; gimnastika zaly; hassalar üçin 8 sany otag; 1 massaž otagy; 1 howuz (basseýn); 1 kafe.
+                        </p>'
+        ],
+        'about_us_section' => [
+            'title' => 'Olimpiýa şäherçesi barada ',
+            'desc' => 'По поручению нашего уважаемого Президента, в нашей стране проведена масштабная работа по развитию физкультурно-оздоровительного движения, массового спорта и спортсменов с высокими достижениями, подготовке туркменских спортсменов к крупным международным соревнованиям, город Ашхабад полностью соответствует международным стандартам, не имеет аналогов во всем регионе и является ярким символом Государственной политики, по обеспечению благополучного и стабильного образа жизни своего народа. Олимпийская деревня стала особым украшением нашего мраморного города, образуя свое неповторимое единство с неповторимой своей красотой и другими объектами спорта и физкультуры, расположенными в нем. В декабре 2017 года была создана Администрация Олимпийского города администрации города Ашхабада.',
+        ],
+        'gallery_section' => 'Fotogalereýa',
+        'news_section' => 'Habarlar',
+    ],
 ];
